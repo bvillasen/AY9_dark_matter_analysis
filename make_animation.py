f93977b9c1a05f6e89f2f4069195233b2e834f68
bruno <bvillasen@gmail.com>
2019-01-29 16:09:18 -0800

    adde make_animation script

---
+++ make_animation.py
@@ -0,0 +1,17 @@
+import sys, time, os
+import numpy as np
+import matplotlib.pyplot as plt
+from subprocess import call
+from shutil import copyfile
+
+inDir =
+outDir = inDir
+image_name = 'particles'
+out_anim_name = 'animation_particles'
+
+cmd = 'ffmpeg -framerate 1 -i '
+cmd += '{0}{1}_%d.png '.format( inDir, image_name )
+cmd += '-pix_fmt yuv420p '
+cmd += '{0}{1}.mp4'.format( outDir, out_anim_name )
+cmd += ' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'
+os.system( cmd )
