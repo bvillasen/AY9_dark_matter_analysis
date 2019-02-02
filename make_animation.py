afc4e3a5ab2c26a68d8e6b63b88cb9064789fd4d
bruno <bvillasen@gmail.com>
2019-02-01 20:53:52 -0800

    yuv420p

--- make_animation.py
+++ make_animation.py
@@ -11,7 +11,7 @@ out_anim_name = 'animation_particles'
 
 cmd = 'ffmpeg -framerate 10 -i '
 cmd += '{0}{1}_%d.png '.format( inDir, image_name )
-cmd += '-pix_fmt yuv1080p '
+cmd += '-pix_fmt yuv420p '
 cmd += '{0}{1}.mp4'.format( outDir, out_anim_name )
 cmd += ' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'
 os.system( cmd )
